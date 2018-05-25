1d290f8a53ed24c52deddd46ad1620374dc055b4
firstboen@gmail.com <firstboen@gmail.com>
2018-05-25 09:38:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 var name = "Wilki"
 var height = "74"
+var tinggi = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(parseInt(height) + 1)
+
+console.log("Tinggi:")
+console.log(tinggi+1)
